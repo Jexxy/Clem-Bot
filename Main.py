49bc068bd91eeb06267292ff876cfde986ae417e
Jexxy <69483936+Jexxy@users.noreply.github.com>
2020-09-13 02:40:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,14 +8,13 @@ TOKEN = t.read()
 t.close()
 bot = commands.Bot(command_prefix='--')
 bot.remove_command('help')
-bot.change_presence()
 command.setup(bot)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
-    await bot.change_presence(status=discord.Status.online, activity= discord.Game('Playing hide and seek!'))
+    await bot.change_presence(activity = discord.Game('--help'))
 
 
 bot.run(TOKEN)
