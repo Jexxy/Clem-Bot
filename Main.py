f59d42e4186127baa99b1c00d4421d640fe4e139
Jexxy <69483936+Jexxy@users.noreply.github.com>
2020-09-13 06:32:56 -0400

    Update main.py
    Moves the reddit init to utilities.py

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 import discord
 import os.path
 import random
-import command
-import praw
+from command import setup
 from discord.ext import commands
 
 ###############################################################################################
@@ -15,19 +14,7 @@ TOKEN = t.read()
 t.close()
 bot = commands.Bot(command_prefix='--')
 bot.remove_command('help')
-command.setup(bot)
-
-##################################
-# Creating an instance of reddit #
-##################################
-# Gets the client secret from Redditsecret.txt, makes an instance of reddit
-
-t = open("Redditsecret", "r")
-client_secret1 = t.read()
-t.close
-reddit = praw.Reddit(client_id="k8NE3qqhrsUOCg",
-                     client_secret=client_secret1,
-                     user_agent="windows:Clem_Bot_Meme_Command:v0.1 (by /u/TheJexxyBoi):python3:PRAW")
+setup(bot)
 
 ###################
 # Meat of Main.py #
@@ -37,7 +24,6 @@ reddit = praw.Reddit(client_id="k8NE3qqhrsUOCg",
 async def on_ready():
     print(f'Logged in as: {bot.user.name}')
     print(f'With ID: {bot.user.id}')
-    print(f'You connection with reddit is {reddit.read_only}')
     await bot.change_presence(activity = discord.Game('--help'))
 
 @bot.event
